577cf8c7577a36b27dfde8ee206b0635d7adb01f
casjay <git-admin@casjaysdev.com>
2020-08-24 00:51:52 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -156,7 +156,7 @@ else
   sudo systemctl enable --now postfix >/dev/null 2>&1
 
   # Setup apache2
-  sudo bash -c "$(curl -LSs https://github.com/casjay-templates/default-web-assets/raw/master/setup.sh)"
+  sudo bash -c "$(curl -LSs https://github.com/casjay-templates/default-web-assets/raw/master/setup.sh >/dev/null 2>&1)"
 
   sudo a2enmod proxy_fcgi setenvif access_compat fcgid expires userdir asis autoindex brotli cgid cgi charset_lite data deflate dir env geoip headers http2 lbmethod_bybusyness lua proxy proxy_http2 request rewrite session_dbd speling ssl status vhost_alias xml2enc >/dev/null 2>&1
   a2ensite default-ssl.conf >/dev/null 2>&1
