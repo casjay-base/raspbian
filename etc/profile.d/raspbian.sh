0c72730f1cc9202e83ef9d12582b76fe513e7d27
casjay <git-admin@casjaysdev.com>
2020-08-31 03:49:16 -0400

    Update raspbian.sh

--- etc/profile.d/raspbian.sh
+++ etc/profile.d/raspbian.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-updateconfigs() { sudo update=yes bash -c $(curl -LSs https://github.com/casjay-base/raspbian/raw/master/install.sh); }
+updateconfigs() { sudo update=yes bash -c "$(curl -LSs https://github.com/casjay-base/raspbian/raw/master/install.sh)"; }
