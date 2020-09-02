67f3d0acbe64fe1c7c9ddc56a383b2e64fb7c564
casjay <git-admin@casjaysdev.com>
2020-09-02 01:02:23 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -153,8 +153,10 @@ else
   sudo $APT $APTOPTS $APTINST update >/dev/null 2>&1
   sudo $APT $APTOPTS $APTINST update >/dev/null 2>&1
   sudo $APT $APTOPTS $APTINST install dnsutils net-tools uptimed downtimed mailutils postfix apache2 nginx ntp gnupg cron openssh-server cowsay fortune-mod figlet geany fonts-hack-ttf fonts-hack-otf fonts-hack-web </dev/null >/dev/null 2>&1
-  sudo $APT $APTOPTS $APTINST install samba tmux neofetch vim-nox fish zsh libapache2-mod-fcgid libapache2-mod-geoip libapache2-mod-php </dev/null >/dev/null 2>&1
   sudo $APT $APTOPTS $APTINST install php7.3 php7.3-bcmath php7.3-bz2 php7.3-cgi php7.3-cli php7.3-common php7.3-curl php7.3-dba php7.3-dev php7.3-enchant php7.3-fpm php7.3-gd php7.3-gmp php7.3-imap php7.3-interbase php7.3-intl php7.3-json php7.3-ldap php7.3-mbstring php7.3-mysql php7.3-odbc php7.3-opcache php7.3-pgsql php7.3-phpdbg php7.3-pspell php7.3-readline php7.3-recode php7.3-snmp php7.3-soap php7.3-sqlite3 php7.3-sybase php7.3-tidy php7.3-xml php7.3-xmlrpc php7.3-xsl php7.3-zip >/dev/null 2>&1
+  sudo $APT $APTOPTS $APTINST install samba tmux neofetch vim-nox fish zsh libapache2-mod-fcgid libapache2-mod-geoip libapache2-mod-php </dev/null >/dev/null 2>&1
+  sudo $APT $APTOPTS $APTINST install librrds-perl libhttp-daemon-perl libjson-perl libipc-sharelite-perl libfile-which-perl libsnmp-extension-passpersist-perl </dev/null >/dev/null 2>&1
+  
   # Remove anacron stuff
   sudo rm -Rf /etc/cron.*/0*
 
